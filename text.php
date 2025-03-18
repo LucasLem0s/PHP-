25edb8ffef49d66fc65fb39bd1b1a8107cdb9db4
Lucas Lemos <46539269+LucasLem0s@users.noreply.github.com>
2025-03-17 22:08:20 -0300

    Add files via upload

---
+++ text.php
@@ -0,0 +1,14 @@
+<?php
+
+$nome = "Lucas";
+$idade = 33; 
+$altura = 1.69;
+$work = "Assistente ADM";
+$instituicao = "Certificação Digital.";
+$livro = "Mistério";
+$paginas = 544;
+$dias = 64;
+print "Meu nome é ". $nome . "  tenho " . $idade . " e minha altura é "
+. $altura . " metros. Trabalho como ". $work . " na ". $instituicao .
+" O ultimo livro que li foi de ". $livro. " ele tinha ". $paginas .  " paginas e levei cerca de " . $dias . " dias para ler."
+?>
